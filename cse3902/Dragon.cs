ff5fa4368d48344fb354355b4348b7ce1cc5878d
Wei Luo <weiluo20021126@gmail.com>
2024-02-01 16:20:03 -0500

    add enemy classes

---
+++ cse3902/Dragon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cse3902
+{
+    public class Dragon : IEnemy
+    {
+        public void move()
+        {
+
+        }
+
+        public void attack()
+        {
+
+        }
+
+        public void takeDmg()
+        {
+
+        }
+    }
+}
---
+++ cse3902/RedFox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cse3902
+{
+    public class RedFox : IEnemy
+    {
+        public void move()
+        {
+
+        }
+
+        public void attack()
+        {
+
+        }
+
+        public void takeDmg()
+        {
+
+        }
+    }
+}
