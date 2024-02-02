904da875478f883cd073aa490372e95acf750793
Wei Luo <weiluo20021126@gmail.com>
2024-02-02 17:47:11 -0500

    added constructors and a rough implementation of method move

--- cse3902/Bat.cs
+++ cse3902/Bat.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,33 @@ namespace cse3902
 {
     public class Bat : IEnemy
     {
+        ISprite sprite;
+
+        public Bat(ContentManager content)
+        {
+            sprite = new Sprite();
+            sprite.texture = content.Load<Texture2D>("enemies");
+        }
+
         public void move()
         {
-
+            Random random = new Random();
+            int randomNum = random.Next(1, 4);
+            switch (randomNum)
+            {
+                case 1:
+                    sprite.moveUp();
+                    break;
+                case 2:
+                    sprite.moveLeft();
+                    break;
+                case 3:
+                    sprite.moveRight();
+                    break;
+                case 4:
+                    sprite.moveDown();
+                    break;
+            }
         }
 
         public void attack()
--- cse3902/Skeleton.cs
+++ cse3902/Skeleton.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +11,38 @@ namespace cse3902
 {
     public class Skeleton : IEnemy
     {
+        ISprite sprite;
+
+        public Skeleton(ContentManager content)
+        {
+            sprite = new Sprite();
+            sprite.texture = content.Load<Texture2D>("enemies");
+        }
+
         public void move()
         {
-
+            Random random = new Random();
+            int randomNum = random.Next(1, 4);
+            switch (randomNum)
+            {
+                case 1:
+                    sprite.moveUp();
+                    break;
+                case 2:
+                    sprite.moveLeft();
+                    break;
+                case 3:
+                    sprite.moveRight();
+                    break;
+                case 4:
+                    sprite.moveDown();
+                    break;
+            }
         }
 
         public void attack()
         {
-
+            // Skeleton does not attack
         }
 
         public void takeDmg()
