91f62c224dfff6bf14110916fb227201d6f57e36
vivian <lu279463768@gmail.com>
2024-02-26 15:14:04 -0500

    add room

--- cse3902/Level.cs
+++ cse3902/Level.cs
@@ -10,6 +10,7 @@ namespace cse3902
     public class Level
     {
         private Player player;
+
         private List<IEnemy> enemies;
         private int idxEnemy;
         private List<IItemPickup> items;
@@ -21,84 +22,13 @@ namespace cse3902
             player = new Player(content);
             player.Position = new Vector2(100, 100);
 
-            enemies = new List<IEnemy>()
-            {
-                new Skeleton(content),
-                new Dragon(content),
-                new Gel(content),
-                new Keese(content),
-                new Goriya(content),
-            };
-            idxEnemy = 0;
-
-            items = new List<IItemPickup>
-            {
-                new FiveRupiesItemPickup(content),
-                new FireItemPickUp(content),
-                new RupyItemPickup(content),
-                new TriforceItemPickup(content),
-                new MapItemPickup(content),
-                new KeyItemPickup(content),
-                new HeartItemPickup(content),
-                new HeartContainerItemPickup(content),
-                new FairyItemPickup(content),
-                new CompassItemPickUp(content),
-                new ClockItemPickUp(content),
-                new BowItemPickup(content),
-                new YellowBoomerangItemPickup(content),
-                new BombItemPickup(content),
-                new LifePotionItemPickup(content),
-                new SecondPotionItemPickup(content),
-                new LetterItemPickup(content),
-                new FoodItemPickup(content),
-                new SwordItemPickup(content),
-                new WhiteSwordItemPickup (content),
-                new MagicalSwordItemPickup(content),
-                new MagicalShieldItemPickup(content),
-                new BlueCandleItemPickup(content),
-                new RedCandleItemPickup (content),
-                new RedRingItemPickup(content),
-                new BlueRingItemPickup (content),
-                new PowerBraceletItemPickup(content),
-                new RecorderItemPickup(content),
-                new RaftItemPickup(content),
-                new StepLadderItemPickup(content),
-                new MagicalRodItemPickup(content),
-                new BookOfMagicItemPickup(content),
-                new MagicalKeyItemPickup(content),
-            };
-            idxItem = 0;
-
-            block = new Block(content);
-
-            /* TODO: create items */
+             
         }
 
         public void Update(GameTime gameTime, IController controller)
         {
-            if (controller.isEnemyPressP())
-            { 
-                idxEnemy++;
-                idxEnemy %= enemies.Count;
-            }
-            if (controller.isEnemyPressO())
-            {
-                idxEnemy--;
-                if (idxEnemy < 0) idxEnemy = enemies.Count - 1;
-            }
-
-            if (controller.isNextItemKeyPress())
-            {
-                idxItem++;
-                idxItem %= items.Count;
-            }
-
-            if(controller.isPreviousItemKeyPress()) 
-            {
-                idxItem--;
-                if (idxItem < 0) idxItem = items.Count - 1;
-            }
-
+           
+            //add room update
             player.Update(gameTime, controller);
             enemies[idxEnemy].Update(gameTime);
             items[idxItem].Update(gameTime);
---
+++ cse3902/Room.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using cse3902.Interfaces;
+using cse3902.Enemy;
+using cse3902.Objects;
+using System.Numerics;
+
+namespace cse3902
+{
+
+    public class Room
+	{
+        private List<IEnemy> enemies;
+        private int idxEnemy;
+        private List<IItemPickup> items;
+        private int idxItem;
+        private IBlock block;
+        public Room(GameContent content, IController controller)
+	    {
+            enemies = new List<IEnemy>()
+            {
+                new Skeleton(content),
+                new Dragon(content),
+                new Gel(content),
+                new Keese(content),
+                new Goriya(content),
+            };
+            idxEnemy = 0;
+
+            items = new List<IItemPickup>
+            {
+                new FiveRupiesItemPickup(content),
+                new FireItemPickUp(content),
+                new RupyItemPickup(content),
+                new TriforceItemPickup(content),
+                new MapItemPickup(content),
+                new KeyItemPickup(content),
+                new HeartItemPickup(content),
+                new HeartContainerItemPickup(content),
+                new FairyItemPickup(content),
+                new CompassItemPickUp(content),
+                new ClockItemPickUp(content),
+                new BowItemPickup(content),
+                new YellowBoomerangItemPickup(content),
+                new BombItemPickup(content),
+                new LifePotionItemPickup(content),
+                new SecondPotionItemPickup(content),
+                new LetterItemPickup(content),
+                new FoodItemPickup(content),
+                new SwordItemPickup(content),
+                new WhiteSwordItemPickup (content),
+                new MagicalSwordItemPickup(content),
+                new MagicalShieldItemPickup(content),
+                new BlueCandleItemPickup(content),
+                new RedCandleItemPickup (content),
+                new RedRingItemPickup(content),
+                new BlueRingItemPickup (content),
+                new PowerBraceletItemPickup(content),
+                new RecorderItemPickup(content),
+                new RaftItemPickup(content),
+                new StepLadderItemPickup(content),
+                new MagicalRodItemPickup(content),
+                new BookOfMagicItemPickup(content),
+                new MagicalKeyItemPickup(content),
+            };
+            idxItem = 0;
+
+            block = new Block(content);
+        }
+
+        public void Update(GameTime gameTime, IController controller)
+        {
+            if (controller.isEnemyPressP())
+            {
+                idxEnemy++;
+                idxEnemy %= enemies.Count;
+            }
+            if (controller.isEnemyPressO())
+            {
+                idxEnemy--;
+                if (idxEnemy < 0) idxEnemy = enemies.Count - 1;
+            }
+
+            if (controller.isNextItemKeyPress())
+            {
+                idxItem++;
+                idxItem %= items.Count;
+            }
+
+            if (controller.isPreviousItemKeyPress())
+            {
+                idxItem--;
+                if (idxItem < 0) idxItem = items.Count - 1;
+            }
+
+          
+            enemies[idxEnemy].Update(gameTime);
+            items[idxItem].Update(gameTime);
+            block.Update(gameTime, controller);
+        }
+
+
+    }
+}
+
