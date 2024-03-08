ecf8c0f4f1d4da69d9fe25b4898a2ea9683f8f0d
PengyuChen01 <121841974+PengyuChen01@users.noreply.github.com>
2024-03-08 00:30:25 -0500

    complete detector

--- cse3902/Collision/CollisionDetector.cs
+++ cse3902/Collision/CollisionDetector.cs
@@ -3,6 +3,7 @@ using cse3902.Interfaces;
 using cse3902.Objects;
 using cse3902.WallClasses;
 using Microsoft.Xna.Framework;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace cse3902;
 
@@ -10,12 +11,19 @@ public static class CollisionDetector
 {
     public static List<CollisionResult<IEnemy>> DetectEnemyCollision(ICollider self, List<IEnemy> enemies)
     {
+        List<CollisionResult<IEnemy>> results = new List<CollisionResult<IEnemy>>();
         foreach (IEnemy enemy in enemies) {
+            if (self.IsColliding(enemy.Collider))
+            {
+                Vector2 depth = self.GetOverlap(enemy.Collider);
+                CollisionResult<IEnemy> result = new CollisionResult<IEnemy>(depth, enemy.Collider, enemy);
+                results.Add(result);
+            }
             /* TODO: get the overlap between the "self" collider and the enemy's collider, and use it to construct a new CollisionResult instance */
             /* TODO: add the collision result instance to a list */
         }
         /* TODO: return the list */
-        return new List<CollisionResult<IEnemy>>();
+        return results;
     }
 
     public static List<CollisionResult<Block>> DetectBlockCollision(ICollider self, List<Block> blocks)
@@ -32,10 +40,20 @@ public static class CollisionDetector
     }
 
     /* TODO: add other collision detection methods */
-    public static List<CollisionResult<Wall>> DetectWallCollision(ICollider self, List<Wall> walls)
+    public static List<CollisionResult<Wall>> DetectWallCollision(ICollider self, Wall wall)
     {
         List<CollisionResult<Wall>> results = new List<CollisionResult<Wall>>();
-        /* TODO */
+        foreach (BoxCollider w in wall.colliders)
+        {
+          
+                if (self.IsColliding(w))
+                {
+                    Vector2 depth = self.GetOverlap(w);
+                    CollisionResult<Wall> result = new CollisionResult<Wall>(depth, w,wall);
+                    results.Add(result);
+                }
+            
+        }
         return results;
     }
 }
