85c34ee30cc8fd0b7cf69b496c1f9d5c644e1bfe
dgoorskey <dersu.goorskey@gmail.com>
2024-03-07 23:30:03 -0500

    fix merge conflict

--- cse3902/Collision/CollisionResolver.cs
+++ cse3902/Collision/CollisionResolver.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Numerics;
 using cse3902.Interfaces;
 using cse3902.Objects;
 using Microsoft.Xna.Framework;
@@ -57,13 +56,40 @@ public static class CollisionResolver
         }
         /* call something like OnHit() on the projectile, passing in the enemy (or list of enemies? or the collision result?) */
         projectile.IsDead = true;
-        results[0].Entity.TakeDmg(1);
+        foreach (CollisionResult<IEnemy> r in results) {
+            r.Entity.TakeDmg(1);
+        }
     }
 
     public static void ResolvePlayerBlockCollision(IPlayer player, List<CollisionResult<Block>> results)
     {
+        /* if no collisions occurred, do nothing */
+        if (results.Count == 0) {
+            return;
+        }
 
         /* TODO: get collision result with largest area and use it to calculate how much to move the player back */
+
+        /* get collision result with largest overlap area */
+        float area = 0f;
+        CollisionResult<Block> biggestResult = results[0];
+        foreach (CollisionResult<Block> result in results)
+        {
+            if (result.GetArea() > area) {
+                area = result.GetArea();
+                biggestResult = result;
+            }
+        }
+
+        /* determine movement amount based on smallest axis of overlap area */
+        Vector2 reconciliation = new Vector2(0, 0);
+        if (biggestResult.Size.X < biggestResult.Size.Y) {
+            reconciliation.X = biggestResult.Size.X;
+        } else {
+            reconciliation.Y = biggestResult.Size.Y;
+        }
+
+        /* determine direction based on relative positions of colliders */
     }
    
 }
