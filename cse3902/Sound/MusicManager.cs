72f3f3c9bde37b93a0d845335de34bae59bcf971
Haosen Fang <fang.957@buckeyemail.osu.edu>
2024-03-25 14:37:48 -0400

    add sound effects

---
+++ cse3902/Sound/MusicManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace cse3902
+{
+    class SoundManager
+    {
+       
+    }
+}
